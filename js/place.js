ad9cc44de450bcf6f5e0d13fd9d707af1d011ce1
giulia notaro <notarogiulia10@gmail.com>
2021-11-30 09:54:07 +0100

    first commit

---
+++ js/place.js
@@ -0,0 +1,14 @@
+export const place = [
+  
+    {  name:" biancavilla", 
+       value: "biancavilla" 
+     },
+   
+     { name: "adrano", 
+       value: "adrano" 
+     },
+   
+     { name: "catania", 
+       value: "catania" 
+     },
+   ];
