8ef18f2c68a418510cdb07fb76c080c325a4ebde
giulia notaro <notarogiulia10@gmail.com>
2021-11-30 23:27:32 +0100

    feat: test element

---
+++ js/data.js
@@ -0,0 +1,238 @@
+let place = [{
+
+    name: "Aci Bonaccorsi", 
+    value: "aci+bonaccorsi"
+},
+{
+    name: "Aci Castello", 
+    value: "aci+castello"
+},
+{
+    name: "ACI CATENA", 
+    value: "aci+catena"
+},
+{
+    name: "Aci Sant'Antonio", 
+    value: "aci+sant'antionio"
+},
+{
+    name: "Acireale", 
+    value: "acireale"
+},
+{
+    name: "Adrano", 
+    value: "adrano"
+},
+{
+    name: "Belpasso", 
+    value: "belpasso"
+},
+{
+    name: "Biancavilla", 
+    value: "biancavilla"
+},
+{
+    name: "Bronte", 
+    value: "bronte"
+},
+{
+    name: "Calatabiano", 
+    value: "calatabiano"
+},
+{
+    name: "Caltagirone", 
+    value: "caltagirone"
+},
+{
+    name: "Camporotondo Etneo", 
+    value: "camporotondo+etneo"
+},
+{
+    name: "Castel di iudica", 
+    value: "castel+di+iudica"
+},
+{
+    name: "Castiglione di Sicilia", 
+    value: "castiglione+di+sicilia"
+},
+{
+    name: "Fiumefreddo", 
+    value: "fiumefreddo"
+},
+{
+    name: "Giarre", 
+    value: "giarre"
+},
+{
+    name: "Giammichele", 
+    value: "giammichele"
+},
+{
+    name: "Gravina di Catania", 
+    value: "gravina+di+catania"
+},
+{
+    name: "Licodia Eubea", 
+    value: "licodia+eubea"
+},
+{
+    name: "Linguaglossa", 
+    value: "linguaglossa"
+},
+{
+    name: "Maletto", 
+    value: "maletto"
+},
+{
+    name: "Maniace", 
+    value: "maniace"
+},
+{
+    name: "Mascali", 
+    value: "mascali"
+},
+{
+    name: "Mascalucia", 
+    value: "mascalucia"
+},
+{
+    name: "Mazzarone", 
+    value: "mazzarone"
+},
+{
+    name: "Militello in val di Catania", 
+    value: "militello+in+val+di+catania"
+},
+{
+    name: "Milo", 
+    value: "milo"
+},
+{
+    name: "Mineo", 
+    value: "mineo"
+},
+{
+    name: "Mirabella Imbaccari", 
+    value: "mirabella+imbaccari"
+},
+{
+    name: "Misterbianco", 
+    value: "misterbianco"
+},
+{
+    name: "Motta Sant'Anastasia", 
+    value: "motta+sant'anastasia"
+},
+{
+    name: "Nicolosi", 
+    value: "nicolosi"
+},
+{
+    name: "Palagonia", 
+    value: "palagonia"
+},
+{
+    name: "Paternò", 
+    value: "paternò"
+},
+{
+    name: "Pedara", 
+    value: "pedara"
+},
+{
+    name: "Piedimonte Etneo", 
+    value: "piedimonte+etneo"
+},
+{
+    name: "Raddusa", 
+    value: "raddusa"
+},
+{
+    name: "Ragalna", 
+    value: "ragalna"
+},
+{
+    name: "Ramacca", 
+    value: "ramacca"
+},
+{
+    name: "Randazzo", 
+    value: "randazzo"
+},
+{
+    name: "Riposto", 
+    value: "riposto"
+},
+{
+    name: "San Cono", 
+    value: "san+cono"
+},
+{
+    name: "San Giovanni la Punta", 
+    value: "san+giovanni+la+punta"
+},
+{
+    name: "San Gregorio di Catania", 
+    value: "san+gregorio+di+catania"
+},
+{
+    name: "San Michele di Ganzaria", 
+    value: "san+michele+di+ganzaria"
+},
+{
+    name: "San Pietro Clarenza", 
+    value: "san+pietro+clarenza"
+},
+{
+    name: "Santa Maria di Licodia", 
+    value: "santa+maria+di+licodia"
+},
+{
+    name: "Santa Venerina", 
+    value: "santa+venerina"
+},
+{
+    name: "Sant'Agata Li Battiati", 
+    value: "sant'agata+li+battiati"
+},
+{
+    name: "Sant'Alfio", 
+    value: "sant'alfio"
+},
+{
+    name: "Scordia", 
+    value: "scordia"
+},
+{
+    name: "Trecastagni", 
+    value: "trecastagni"
+},
+{
+    name: "Tremestieri Etneo", 
+    value: "tremestieri+etneo"
+},
+{
+    name: "Valverde", 
+    value: "valverde"
+},
+{
+    name: "Viagrande", 
+    value: "viagrande"
+},
+{
+    name: "Vizzini", 
+    value: "vizzini"
+},
+{
+    name: "Zafferana Etnea", 
+    value: "zafferana+etnea"
+},
+
+
+
+
+];
+
+
+
+export {place};
--- js/script.js
+++ js/script.js
@@ -1,4 +1,59 @@
+import { place } from "./data.js";
 
+/* const API = { apiKey : `97cb9169215648941878bdef535de1fd`,
+              weatherUrl : `https://api.openweathermap.org/data/2.5/weather?`
+} */
 
+const choosePlace = document.getElementById("choosePlace");
+const nameOfCity = document.querySelector(`#name`);
+const temp = document.querySelector(".main");
+const description = document.querySelector(".description");
+const humidity = document.querySelector(".humidity");
+const card = document.querySelector(".card");
 
+choosePlace.innerHTML = place.reduce(
+  (options, { value, name }) =>
+    (options += `<option value="${value}">${name}</option>`),
+  '<option value="" selected></option>'
+);
 
+choosePlace.addEventListener("change", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      choosePlace.value +
+      "&units=metric&appid=97cb9169215648941878bdef535de1fd"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const tempValue = data["main"]["temp"] 
+      const nameValue = data["name"];
+      const descValue = data["weather"][0]["description"];
+      const humValue = data["main"]["humidity"];
+      const windValue = data["wind"] ["speed"];
+
+      let temperature = (Math.floor(`${tempValue}`));
+      let wind = (Math.floor(`${windValue}`));
+      
+      
+
+      let pElement = document.querySelectorAll("div > p")[0];
+
+      while (pElement.firstChild) pElement.removeChild(pElement.firstChild);
+      pElement.appendChild(
+       document.createElement ("p") ( `  ${nameValue} 
+            
+            weather: ${descValue} 
+                  
+            speed's wind: ${wind} 
+
+            temperature: ${temperature}°C
+            
+            
+            humidity: ${humValue}%
+
+          
+           ` 
+        )
+      );
+    });
+});
